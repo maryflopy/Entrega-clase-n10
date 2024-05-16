f713884f6a0de4146cd92da1e6571c093c2d66b8
florencia <166770245+maryflopy@users.noreply.github.com>
2024-05-16 19:47:03 -0300

    Agregando archivos js css

---
+++ funcionalidades.js
@@ -0,0 +1,45 @@
+alert("MariaFlorenciaRuiz");
+
+var nombre = "Maria Florencia";
+var apellido = " Ruiz";
+var resultado;
+resultado = nombre + apellido
+console.log(nombre + "" + apellido);
+
+var numero1= 6;
+var numero2= 8;
+var resultado= numero1 + numero2;
+console.log("suma:",resultado);
+
+var numero3= 15;
+var resultado= ("suma:",resultado + numero3);
+console.log(resultado);
+
+var numero1= "Florencia";
+var numero2= 30;
+var resultado = numero1 - numero2;
+console.log(resultado);
+
+var variable1= "13";
+var variable2= 13;
+var resultado;
+resultado = variable1 === variable2
+console.log(resultado);
+
+var variable1= "13";
+var variable2= 25;
+resultado = variable1 != variable2
+console.log(resultado);
+
+var variable1= "13";
+var variable2= 13;
+var resultado;
+resultado = variable1 == variable2
+console.log(resultado);
+
+var variable1= "13";
+var variable2= -13;
+var resultado;
+resultado = variable1 === variable2
+console.log(resultado);
+
